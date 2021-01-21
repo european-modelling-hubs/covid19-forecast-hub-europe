af8d57b06560f5a4814d8e01fa1be3660bf8d9a3
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-01-21 14:27:09 +0000

    update ECDC downloader

--- code/auto_download/ecdc_download.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-print('Downloading data from ECDC.')
-df = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
-#df = pd.read_excel('https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx')
-
-current_date = pd.to_datetime('today').date()
-df.to_csv('../../data-truth/ECDC/raw/' + str(current_date) + '-Deaths-ECDC.csv', index=False)
