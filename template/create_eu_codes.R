51943e0fdcd0384a2d91ef2eccd7bac0068f6ee4
kathsherratt <kathsherratt@github.com>
2021-01-21 20:29:44 +0000

    iso3
    use correct location codes (ISO-3)

--- template/create_eu_codes.R
+++ template/create_eu_codes.R
@@ -5,7 +5,7 @@ eu28 <- countrycode::codelist
 eu28 <- eu28[!is.na(eu28$eu28),]
 
 eu28 <- dplyr::select(eu28, 
-                      eu28, continent, iso.name.en, iso2c, 
-                      country.name.en.regex, eurostat, fao, fips,  iso3c)
+                      eu28, continent, iso.name.en, iso3c, 
+                      country.name.en.regex, eurostat, fips)
 
 readr::write_csv(eu28, "locations_eu.csv")
