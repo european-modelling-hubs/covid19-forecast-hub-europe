9d458997d1cd959c1981891b37708606d4e862cd
nikosbosse <nikosbosse@gmail.com>
2021-03-15 14:28:38 +0100

    update report and report structure

---
+++ evaluation-report/report.R
@@ -0,0 +1,22 @@
+# packages ---------------------------------------------------------------------
+library(purrr)
+library(dplyr)
+library(here)
+library(readr)
+library(scoringutils)
+library(rmarkdown)
+library(data.table)
+library(covidHubUtils)
+
+
+
+rmarkdown::render(here::here("evaluation-report", "report-template.Rmd"),
+                  output_format = "html_document",
+                  output_file = here::here("docs", "index.html"),
+                  envir = new.env(),
+                  clean = TRUE)
+
+
+## to make this generalisable
+# allow bits to be turned off and on
+# somehow pass down the filtering
