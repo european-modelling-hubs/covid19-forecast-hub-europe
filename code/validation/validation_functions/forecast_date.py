5e957c42da43f449699489dfae5ff57fec440f74
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-03-16 14:27:52 +0000

    report more meaningful header errors to fix #107 (#126)

--- code/validation/validation_functions/forecast_date.py
+++ code/validation/validation_functions/forecast_date.py
@@ -5,14 +5,17 @@ import os
 def filename_match_forecast_date(filepath):
     df = pd.read_csv(filepath)
     file_forecast_date = os.path.basename(os.path.basename(filepath))[:10]
-    forecast_date_column = set(list(df['forecast_date']))
-    if len(forecast_date_column) > 1:
-        return True, ["FORECAST DATE ERROR: %s has multiple forecast dates: %s. Forecast date must be unique" % (
-            filepath, forecast_date_column)]
-    else:
-        forecast_date_column = forecast_date_column.pop()
-        if (file_forecast_date != forecast_date_column):
-            return True, ["FORECAST DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
-                filepath, file_forecast_date, forecast_date_column)]
+    if 'forecast_date' in df:
+        forecast_date_column = set(list(df['forecast_date']))
+        if len(forecast_date_column) > 1:
+            return True, ["FORECAST DATE ERROR: %s has multiple forecast dates: %s. Forecast date must be unique" % (
+                filepath, forecast_date_column)]
         else:
-            return False, "no errors"
+            forecast_date_column = forecast_date_column.pop()
+            if (file_forecast_date != forecast_date_column):
+                return True, ["FORECAST DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
+                    filepath, file_forecast_date, forecast_date_column)]
+            else:
+                return False, "no errors"
+    else :
+        return True, ["FORECAST DATE ERROR: column forecast_date not found"]
