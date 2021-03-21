362f72d2cbbe27f777994a9d5dbd82476e0fbd3c
nikosbosse <nikosbosse@gmail.com>
2021-03-21 11:52:16 +0100

    reduce plot size and adapt report

--- evaluation-report/report.R
+++ evaluation-report/report.R
@@ -10,7 +10,7 @@ library(covidHubUtils)
 
 rmarkdown::render(here::here("reports", "evaluation", "report-template.Rmd"),
                   output_format = "html_document",
-                  output_file = here::here("docs", "evaluation.html"),
+                  output_file = here::here("docs", "index.html"),
                   envir = new.env(),
                   clean = TRUE)
 
