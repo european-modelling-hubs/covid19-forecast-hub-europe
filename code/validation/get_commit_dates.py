375c540c0be81475261ceac1743560dbd97624ed
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-03-28 09:23:32 +0100

    restore commit date script
    previously removed in 360b516e24848057b6502735b1ef07e314a96889

---
+++ code/validation/get_commit_dates.py
@@ -0,0 +1,17 @@
+import git
+from pyprojroot import here
+import pandas as pd
+
+repo = git.Repo(str(here()))
+tree = repo.tree()
+
+commit_dates = pd.DataFrame(columns=['filename', 'first_commit', 'latest_commit'])
+
+for blob in tree.traverse():
+    if (blob.path.startswith('data-processed') & blob.path.endswith('.csv')):
+        commits = list(repo.iter_commits(paths=blob.path))
+        commit_dates.loc[len(commit_dates)] = [blob.path.split("/")[-1], # filename
+                                               str(pd.to_datetime(commits[-1].committed_date, unit='s').date()), # first commit
+                                               str(pd.to_datetime(commits[0].committed_date, unit='s').date())] # latest commit
+        
+commit_dates.to_csv(here('./code/validation/commit_dates.csv'), index=False)
