7ae623f7fd236e0b84a443fe2a95e5fc2a960f99
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-03-30 11:01:00 +0100

    render to new file names

--- reports/compile-reports.r
+++ reports/compile-reports.r
@@ -7,18 +7,23 @@ library(scoringutils)
 library(rmarkdown)
 library(data.table)
 library(covidHubUtils)
+library(lubridate)
 
-rmarkdown::render(here::here("reports", "evaluation", "report-template.Rmd"),
+options(knitr.duplicate.label = "allow")
+
+date <- lubridate::floor_date(lubridate::today(), 'week', week_start = 1)
+
+rmarkdown::render(here::here("reports", "evaluation", "evaluation-report.Rmd"),
                   output_format = "html_document",
-                  output_file = here::here("docs", "evaluation.html"),
-                  envir = new.env(),
-                  clean = FALSE)
+                  output_file =
+                    here::here("docs", paste0("evaluation-report-", date, ".html")),
+                  envir = new.env())
 
 rmarkdown::render(here::here("reports", "ensemble", "ensemble-report.Rmd"),
                   output_format = "html_document",
-                  output_file = here::here("docs", "ensemble.html"),
-                  envir = new.env(),
-                  clean = FALSE)
+                  output_file =
+                    here::here("docs", paste0("ensemble-report-", date, ".html")),
+                  envir = new.env())
 
 ## to make this generalisable
 # allow bits to be turned off and on
