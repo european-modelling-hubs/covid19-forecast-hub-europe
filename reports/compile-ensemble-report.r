7a0bc26fe3f1f3adc530ddc1d2a1055fe9e404d1
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-04-07 19:18:37 +0100

    fix location of ensemble report

--- reports/compile-ensemble-report.r
+++ reports/compile-ensemble-report.r
@@ -13,13 +13,13 @@ options(knitr.duplicate.label = "allow")
 
 report_date <- lubridate::floor_date(lubridate::today(), 'week', week_start = 1)
 
-locations <- hub_locations_ecdc
+dir.create(here::here("html"))
 
 rmarkdown::render(here::here("reports", "ensemble", "ensemble-report.Rmd"),
                   params = list(report_date = report_date),
                   output_format = "html_document",
                   output_file =
-                    here::here("docs", paste0("ensemble-report-", report_date, ".html")),
+                    here::here("html", paste0("ensemble-report-", report_date, ".html")),
                   envir = new.env())
 
 ## to make this generalisable
