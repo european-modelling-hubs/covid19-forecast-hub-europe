ef99cc9c3ab01a6b0ae218fb087d050689ab6e2e
kathsherratt <kathsherratt@gmail.com>
2021-04-08 18:54:54 +0100

    update github action

--- code/ensemble/utils/create-manual-exclusions.R
+++ code/ensemble/utils/create-manual-exclusions.R
@@ -1,18 +1,18 @@
 library(here)
 
-exclude <- readr::read_csv(here("code/ensemble/EuroCOVIDhub/manual-exclusions.csv"),
+exclude <- vroom::vroom(here("code/ensemble/EuroCOVIDhub/manual-exclusions.csv"),
                            col_types = "ccc")
 
 exclude_new <- tibble::tribble(
-  
+
   # Replace NAs here to add models as necessary
-  
+
   ~forecast_date, ~model, ~reason_for_exclusion,
-  
+
   NA, NA, NA
-  
+
 )
 
 exclude <- dplyr::bind_rows(exclude, exclude_new)
 
-readr::write_csv(exclude, here("code/ensemble/EuroCOVIDhub/manual-exclusions.csv"))
+vroom::vroom_write(exclude, here("code/ensemble/EuroCOVIDhub/manual-exclusions.csv"))
