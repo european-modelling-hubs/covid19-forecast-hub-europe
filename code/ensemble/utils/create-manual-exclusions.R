4e7c37066cd93fd5058fd149f832d3b72d4ff31e
kathsherratt <kathsherratt@gmail.com>
2021-04-08 17:45:17 +0100

    add simple files for method and manual exclusion

---
+++ code/ensemble/utils/create-manual-exclusions.R
@@ -0,0 +1,18 @@
+library(here)
+
+exclude <- readr::read_csv(here("code/ensemble/manual-exclusions.csv"),
+                           col_types = "ccc")
+
+exclude_new <- tibble::tribble(
+  
+  # Replace NAs here to add models as necessary
+  
+  ~forecast_date, ~model, ~reason_for_exclusion,
+  
+  NA, NA, NA
+  
+)
+
+exclude <- dplyr::bind_rows(exclude, exclude_new)
+
+readr::write_csv(exclude, here("code/ensemble/manual-exclusions.csv"))
