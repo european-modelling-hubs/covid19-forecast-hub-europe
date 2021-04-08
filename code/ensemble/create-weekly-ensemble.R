6795b0e37785f2c2f02b04227bac86669b33f43e
kathsherratt <kathsherratt@gmail.com>
2021-04-08 17:49:58 +0100

    Create create-weekly-ensemble.R

---
+++ code/ensemble/create-weekly-ensemble.R
@@ -0,0 +1,40 @@
+# Runs and formats EuroCOVIDhub ensemble 
+#  saved in data-processed/EuroCOVIDhub-ensemble
+library(vroom)
+library(dplyr)
+library(lubridate) 
+source(here("code", "ensemble", "utils", "run-ensemble.R"))
+
+# Set up ----------------------------------------------------------------
+# Get method
+method <- readLines(here("code", "ensemble", "current-method.txt"))
+
+# Set current week of possible forecast dates
+forecast_dates <- seq.Date(from = floor_date(today(), "week", 1), 
+                           by = -1,
+                           length.out = 6)
+
+# Get model names for manual exclusion
+exclude_models <- vroom(here("code", "ensemble", "manual-exclusions.csv")) %>%
+  filter(forecast_date %in% forecast_dates) %>%
+  pull(model)
+
+# Run weekly automated ensemble -----------------------------------------
+hub_ensemble <- run_ensemble(method = method,
+                             forecast_date = forecast_dates,
+                             exclude_models = exclude_models,
+                             return_criteria = TRUE)
+
+# Save forecast in data-processed
+vroom_write(hub_ensemble$forecast,
+            here("data-processed", 
+                 paste0("EuroCOVIDhub-ensemble"),
+                 paste0(hub_ensemble$forecast_date, 
+                        "-EuroCOVIDhub-ensemble.csv")),
+            delim = ",")
+
+# Save model criteria in code/ensemble
+vroom_write(hub_ensemble$forecast,
+            here("code", "ensemble", "weekly-criteria",
+                 paste0(hub_ensemble$forecast_date, ".csv")),
+            delim = ",")
