391eb714aab6fb876d86082ab95f16e154f1e517
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-04-09 20:22:41 +0100

    create dirs/files if don't exist

--- code/ensemble/EuroCOVIDhub/create-weekly-ensemble.R
+++ code/ensemble/EuroCOVIDhub/create-weekly-ensemble.R
@@ -4,6 +4,8 @@ library(vroom)
 library(dplyr)
 library(lubridate) 
 library(here)
+library(tibble)
+
 source(here("code", "ensemble", "utils", "run-ensemble.R"))
 
 # Set up ----------------------------------------------------------------
@@ -35,16 +37,31 @@ vroom_write(hub_ensemble$ensemble,
 
 
 # Save criteria + methods -------------------------------------------------
+suppressWarnings(
+  dir.create(here("code", "ensemble", "EuroCOVIDhub", "criteria"))
+)
 vroom_write(hub_ensemble$criteria,
             here("code", "ensemble", "EuroCOVIDhub",
                  "criteria",
-                 paste0(hub_ensemble$forecast_date, ".csv")),
+                 paste0("criteria-", hub_ensemble$forecast_date, ".csv")),
             delim = ",")
 
 # Add method to csv
-vroom(here("code", "ensemble", "EuroCOVIDhub", "method.csv")) %>%
+methods_by_date_file <-
+  here("code", "ensemble", "EuroCOVIDhub", "method-by-date.csv")
+if (file.exists(methods_by_date_file)) {
+  methods_by_date <- vroom(methods_by_date_file)
+} else {
+  methods_by_date <- tibble(forecast_date = as.Date(character(0)),
+                            method = character(0))
+}
+methods_by_date %>%
   add_row(forecast_date = hub_ensemble$forecast_date,
           method = method) %>%
+  ## remove all but last entry if dates are multiple
+  group_by(forecast_date) %>%
+  slice(n()) %>%
+  ungroup() %>%
   vroom_write(here("code", "ensemble", "EuroCOVIDhub",
                  "method-by-date.csv"),
             delim = ",")
