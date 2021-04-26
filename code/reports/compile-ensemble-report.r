947fc9d6e47c813cf590a9ca420f1f94ef3f1d19
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-04-26 09:54:16 +0100

    Workflow tidy-up

--- code/reports/compile-ensemble-report.r
+++ code/reports/compile-ensemble-report.r
@@ -11,15 +11,18 @@ library(lubridate)
 
 options(knitr.duplicate.label = "allow")
 
-report_date <- lubridate::floor_date(lubridate::today(), 'week', week_start = 1)
+report_date <-
+  lubridate::floor_date(lubridate::today(), "week", week_start = 7) + 1
 
 dir.create(here::here("html"))
 
-rmarkdown::render(here::here("reports", "ensemble", "ensemble-report.Rmd"),
+rmarkdown::render(here::here("code", "reports", "ensemble",
+                             "ensemble-report.Rmd"),
                   params = list(report_date = report_date),
                   output_format = "html_document",
                   output_file =
-                    here::here("html", paste0("ensemble-report-", report_date, ".html")),
+                    here::here("html", paste0("ensemble-report-", report_date,
+                                              ".html")),
                   envir = new.env())
 
 ## to make this generalisable
