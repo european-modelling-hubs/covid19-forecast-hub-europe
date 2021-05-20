6430be0245c7a96933ec77b0f02a3d982e14b4fb
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2021-05-20 17:49:26 +0530

    Create ensembles using multiple methods/dates (#397)
    Co-authored-by: GitHub Action - Visualisation Update <action@github.com>
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>

---
+++ code/ensemble/EuroCOVIDhub/re-create-weekly-ensembles.R
@@ -0,0 +1,35 @@
+# Re-create past weekly ensembles for EuroCOVIDhub
+library(here)
+library(vroom)
+library(purrr)
+source(here("code", "ensemble", "utils", "run-ensemble.R"))
+
+# Get method by date
+method_by_date <- vroom(here("code", "ensemble", "EuroCOVIDhub",
+                     "method-by-date.csv"))
+
+# Get exclusions by date
+exclude_by_date <- vroom(here("code", "ensemble", "EuroCOVIDhub", 
+                              "manual-exclusions.csv"))
+
+# Run ensemble for past weeks
+ensembles <- map2(.x = method_by_date$method,
+                  .y = method_by_date$forecast_date,
+                  ~ run_ensemble(method = .x, 
+                                 forecast_date = .y,
+                                 exclude_models = exclude_by_date))
+
+# Save forecast in data-processed
+walk(.x = ensembles,
+     ~ vroom_write(.$ensemble,
+            here("data-processed", "EuroCOVIDhub-ensemble",
+                 paste0(.$forecast_date, 
+                        "-EuroCOVIDhub-ensemble.csv")),
+            delim = ","))
+
+# Save criteria
+walk(.x = ensembles,
+     ~ vroom_write(.$criteria,
+                 here("code", "ensemble", "EuroCOVIDhub", "criteria",
+                      paste0("criteria-", .$forecast_date, ".csv")),
+                 delim = ","))
