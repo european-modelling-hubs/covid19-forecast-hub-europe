67210a542cc76d376208453bd6f535cce1d5b959
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2021-06-01 10:19:35 +0100

    add anomalies csv

---
+++ data-truth/anomalies/create-anomalies.R
@@ -0,0 +1,17 @@
+# Create csv with data anomalies
+# 2021-06-01 placeholder before implementing automated detection
+library(here)
+library(dplyr)
+library(tibble)
+
+anomalies <- tribble(
+  ~forecast_date, ~target_variable, ~location, ~location_name, ~problem,
+  "2021-05-24", "case", "FR", "France", "Removed double counting",
+  "2021-05-24", "case", "IE", "Ireland", "No data reported",
+  "2021-05-24", "death", "IE", "Ireland", "No data reported",
+  "2021-05-31", "case", "IE", "Ireland", "No data reported",
+  "2021-05-31", "death", "IE", "Ireland", "No data reported"
+)
+
+vroom_write(anomalies,
+            here("data-truth", "anomalies", "anomalies.csv"))
