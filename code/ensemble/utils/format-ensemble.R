d46c186b6163d8ab18d2104b5a4d045f1e94dae7
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2021-06-01 08:21:19 +0100

    more robust formatting

--- code/ensemble/utils/format-ensemble.R
+++ code/ensemble/utils/format-ensemble.R
@@ -2,7 +2,7 @@
 #
 # Converts a quantile-only ensemble based on covidHubUtils::load_forecasts()
 # to standardised submission format
-# 
+#
 # Steps:
 # Creates "target" variable
 # Adds point forecasts from 0.5 quantile
@@ -10,24 +10,41 @@
 library(dplyr)
 
 format_ensemble <- function(ensemble,
-                            forecast_date) {
-  
-  # Format as standard forecast
+                            forecast_date,
+                            temporal_resolution = "wk") {
+
+  ensemble <- ungroup(ensemble)
+
+  # Add target end date
+  if (!"target_end_date" %in% names(ensemble)) {
+    ensemble <- ensemble %>%
+      mutate(target_end_date = ((forecast_date +
+                                   (7 - wday(forecast_date)) - 7 ) +
+                                (7 * horizon)))
+  }
+
+  # Add type
+  if (!"type" %in% names(ensemble)) {
+    ensemble <- ensemble %>%
+      mutate(type = "quantile")
+  }
+
+  # Set target and model
   ensemble <- ensemble %>%
-    mutate(forecast_date = forecast_date,
-           target = paste(horizon, temporal_resolution, 
+    mutate(forecast_date = !!forecast_date,
+           target = paste(horizon, temporal_resolution,
                           "ahead", target_variable)) %>%
+    # Keep only standard columns
     select(forecast_date, target, target_end_date,
            location, type, quantile, value)
-  
+
   # Add point forecasts
   ensemble_point <- ensemble %>%
     filter(quantile == 0.5) %>%
     mutate(type = "point",
            quantile = NA_real_)
-  
   ensemble_with_point <- ensemble %>%
     bind_rows(ensemble_point)
-  
+
   return(ensemble_with_point)
 }
