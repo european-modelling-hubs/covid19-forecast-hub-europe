8be61d142460c1c6d670c66223098b86664f066b
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2021-06-12 15:02:38 +0530

    Ensemble weighted by inverse relative skill (#467)
    * Create create-ensemble-relative-skill.R
    
    * Update create-ensemble-relative-skill.R
    
    * minor fix to create-ensemble-average
    
    * more robust formatting
    
    * Update run-ensemble.R
    
    * Update run-multiple-ensembles.R
    
    * Update create-all-methods-ensembles.R
    
    * Add past ensembles + weights csv
    
    * average skill over all horizons by default
    
    Added arg `by_horizon`.
    
    TRUE takes relative skill by horizon, model, location, target_variable, then weights and applies to values by horizon.
    
    FALSE (default) takes mean relative skill over all horizons for each model, location, target variable, then weights and applies to values.
    
    * clean up create-all-methods-ensembles
    
    remove test code
    
    * create all past relative_skill ensembles with by_horizon=FALSE
    
    This overwrites all the past ensembles previously created with weights by horizon. Relative skill ensemble weights are now averaged by horizon.
    
    Overwrites contents of `ensembles/data-processed/EuroCOVIDhub-relative_skill` (8 csvs) and `ensembles/weights/EuroCOVIDhub-relative_skill` (8 csvs)
    
    * allow running both rel_skill ensembles
    
    * weighted median ensemble
    
    * mean/median update
    
    * update workflow
    
    * clean whitespace
    
    * correctly save weights
    
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>

--- code/ensemble/methods/create-ensemble-average.R
+++ code/ensemble/methods/create-ensemble-average.R
@@ -1,24 +1,19 @@
 # Create ensemble using mean or median average.
-# 
+#
 # Steps:
 # Takes forecasts which should come from load_ensemble_forecasts()
 # Averages by mean or median
 # Formats in submission format
-# 
+#
 # Returns ensemble model
-# 
+#
 library(dplyr)
 
-create_ensemble_average <- function(forecasts, 
+create_ensemble_average <- function(forecasts,
                                     method = c("mean", "median")) {
-  # Set up
-  ensemble <- forecasts %>%
-    filter(type == "quantile") %>%
-    mutate(quantile = round(quantile, 3))
-  
   # Mean
   if (method == "mean") {
-    ensemble <- ensemble %>%
+    ensemble <- forecasts %>%
       group_by(target_variable, horizon, temporal_resolution,
                target_end_date, location, type, quantile) %>%
       summarise(forecasts = n(),
@@ -26,8 +21,8 @@ create_ensemble_average <- function(forecasts,
                 .groups = "drop")
     # Median
   } else if (method == "median") {
-    ensemble <- ensemble %>%
-      group_by(target_variable, horizon, temporal_resolution, 
+    ensemble <- forecasts %>%
+      group_by(target_variable, horizon, temporal_resolution,
                target_end_date, location, type, quantile) %>%
       summarise(forecasts = n(),
                 value = median(value),
@@ -35,7 +30,7 @@ create_ensemble_average <- function(forecasts,
   } else {
     stop("Supported methods: 'mean' or 'median'")
   }
-  
+
   # Return ensemble
   return(ensemble)
 }
