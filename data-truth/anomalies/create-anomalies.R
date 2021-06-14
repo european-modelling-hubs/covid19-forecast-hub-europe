3461cf1c934ac0d6cdf1d0402276f2720c755da6
kathsherratt <kathsherratt@gmail.com>
2021-06-14 17:23:57 +0100

    Remove anomalies from evaluation
    - Adds anomalies for week ending 2021-06-12
    - This should remove them from evaluation

--- data-truth/anomalies/create-anomalies.R
+++ data-truth/anomalies/create-anomalies.R
@@ -18,7 +18,11 @@ anomalies <- tribble(
   "2021-05-29", "inc death", "IE", "Ireland", "No data reported",
   #
   "2021-06-05", "inc death", "IE", "Ireland", "No data reported",
-  "2021-06-05", "inc case", "IE", "Ireland", "2 days missing, 1 day spike"
+  "2021-06-05", "inc case", "IE", "Ireland", "2 days missing, 1 day spike",
+  #
+  "2021-06-12", "inc death", "IE", "Ireland", "No data reported",
+  "2021-06-12", "inc case", "IE", "Ireland", "Spike removed after case count backdistributed",
+  "2021-06-12", "inc case", "ES", "Spain", "Historic cases added but not backdistributed in Catalonia",
 ) %>%
   mutate(target_end_date = as.Date(target_end_date))
 
