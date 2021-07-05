17ba5c90195ff73e26e4cad61f12ac37792e4f87
kathsherratt <kathsherratt@gmail.com>
2021-07-05 16:29:29 +0100

    minor updates to existing code
    - switch to readr::read_csv instead of vroom
    - use hub location csv instead of countrycode package
    - format with ISO-2 codes not ISO-3

--- code/auto_download/ecdc_download.r
+++ code/auto_download/ecdc_download.r
@@ -1,8 +1,6 @@
 library("here")
-library("vroom")
-library("eurostat")
+library("readr")
 library("dplyr")
-library("countrycode")
 library("tidyr")
 library("ISOweek")
 library("stringi")
@@ -10,12 +8,10 @@ library("lubridate")
 
 cat("Downloading ECDC data\n")
 
-countries <- eurostat::eu_countries %>%
-  bind_rows(eurostat::efta_countries) %>%
-  rename(country = name, eurostat = code) %>%
-  mutate(country_code = countrycode(eurostat, "eurostat", "iso3c")) %>%
-  select(country_code)
+locations <- read_csv(here("data-locations", "locations_eu.csv")) %>%
+  select(location, location_name)
 
+# Case and death data
 ct <- "cc--cic---"
 
 ecdc_dir <- here::here("data-truth", "ECDC")
@@ -24,14 +20,14 @@ file_base <- "truth_ECDC-Incident"
 if (!dir.exists(ecdc_dir)) dir.create(ecdc_dir, recursive = TRUE)
 
 df <-
-  vroom::vroom("https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv",
+  read_csv("https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv",
                col_types = ct) %>%
-  inner_join(countries, by = "country_code") %>%
+  inner_join(locations, by = c("country" = "location_name")) %>%
   separate(year_week, c("year", "week"), sep = "-") %>%
   mutate(week_start = ISOweek2date(paste0(year, "-W", week, "-1"))) %>%
   mutate(indicator = stri_trans_totitle(indicator)) %>%
   select(week_start, indicator,
-         location = country_code, location_name = country,
+         location, location_name = country,
          value = weekly_count) %>%
   group_by(indicator) %>%
   group_walk(~ vroom_write(.x, delim = ",",
@@ -40,27 +36,23 @@ df <-
                                                    .y$indicator, ".csv"))))
 
 # Hospitalisation data
-# Get file
-data_filepath <- here("data-truth", "ECDC", 
+ecdc_hosp_filepath <- here("data-truth", "ECDC", 
                       "truth_ecdc-Incident Hospitalizations.csv") # covidHubUtils format
 
-R.utils::downloadFile(Sys.getenv("DATA_URL"), 
-                      data_filepath, 
+R.utils::downloadFile(Sys.getenv("DATA_URL"), # Uses set environment variables
+                      ecdc_hosp_filepath, 
                       username = Sys.getenv("DATA_USERNAME"), 
                       password = Sys.getenv("DATA_PASSWORD"),
                       skip = FALSE,
                       overwrite = TRUE)
 
-locations <- read_csv(here("data-locations", "locations_eu.csv")) %>%
-  select(location, location_name)
-
-# Format
 public_sources <- c("Country_Website", "Country_API", "Country_Github")
-ecdc <- vroom(data_filepath) %>%
+
+ecdc_hosp <- read_csv(ecdc_hosp_filepath) %>%
   filter(Indicator %in% c("New_Hospitalised") &
            Source %in% public_sources) %>%
   rename(location_name = CountryName) %>%
   mutate(target_variable = "inc hosp") %>%
   left_join(locations, by = "location_name") %>%
   select(date = Date, location, location_name, value = Value) %>%
-  write_csv(file = data_filepath, append = FALSE)
+  write_csv(file = ecdc_hosp_filepath, append = FALSE)
