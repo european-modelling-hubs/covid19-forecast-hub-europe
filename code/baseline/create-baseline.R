634cd2b74392c285936ee612d56b80288699e3e3
Hugo Gruson <hugo.gruson@protonmail.com>
2021-07-06 10:46:41 +0200

    Revert "Edit code to get previous baselines"
    This reverts commit 4e257a3f5f98e4eff360540649ad2932a8046e82.

--- code/baseline/create-baseline.R
+++ code/baseline/create-baseline.R
@@ -19,46 +19,44 @@ hub_quantiles <- get_hub_config("forecast_type")[["quantiles"]]
 hub_horizon <- get_hub_config("horizon")[["value"]]
 hub_targets <- get_hub_config("target_variables")
 
-for (forecast_date in seq(ymd("2021-03-08"), ymd("2021-06-21"), by = 7)) {
+forecast_date <- today()
 
-  forecast_date <- as.Date(forecast_date, origin = "1970-01-01")
-
-  # Wrapper to build baseline because S3 doesn't usually work well with pipe
-  # workflows
-  build_baseline <- function(inc_obs, quantiles, horizon) {
-    baseline_fit <- covidModels::fit_quantile_baseline(inc_obs)
-    predict(
-      baseline_fit,
-      inc_obs,
-      cumsum(inc_obs),
-      quantiles = quantiles,
-      horizon = horizon,
-      num_samples = 100000
-    )
-  }
-
-  raw_truth <- covidHubUtils::load_truth(
-    truth_source = "JHU",
-    target_variable = hub_targets,
-    truth_end_date = forecast_date - 1,
-    hub = "ECDC"
+# Wrapper to build baseline because S3 doesn't usually work well with pipe
+# workflows
+build_baseline <- function(inc_obs, quantiles, horizon) {
+  baseline_fit <- covidModels::fit_quantile_baseline(inc_obs)
+  predict(
+    baseline_fit,
+    inc_obs,
+    cumsum(inc_obs),
+    quantiles = quantiles,
+    horizon = horizon,
+    num_samples = 100000
   )
-
-  baseline_forecast <- raw_truth %>%
-    filter(!is.na(value)) %>%
-    group_by(location, target_variable) %>%
-    group_map(
-      ~ full_join(
-        .y,
-        build_baseline(.x$value, quantiles = hub_quantiles, horizon = hub_horizon),
-        by = character()
-      )
-    ) %>%
-    bind_rows() %>%
-    filter(type %in% substr(hub_targets, 1, 3)) %>%
-    mutate(type = "quantile")
-
-  format_ensemble(baseline_forecast, forecast_date) %>%
-    write_csv(paste0(model_folder, "/", forecast_date, "-", model_name, ".csv"))
-
 }
+
+raw_truth <- covidHubUtils::load_truth(
+  truth_source = "JHU",
+  target_variable = hub_targets,
+  truth_end_date = forecast_date - 1,
+  hub = "ECDC"
+)
+
+baseline_forecast <- raw_truth %>%
+  filter(!is.na(value)) %>%
+  group_by(location, target_variable) %>%
+  group_map(
+    ~ full_join(
+      .y,
+      build_baseline(.x$value, quantiles = hub_quantiles, horizon = hub_horizon),
+      by = character()
+    )
+  ) %>%
+  bind_rows() %>%
+  filter(type %in% substr(hub_targets, 1, 3)) %>%
+  mutate(type = "quantile")
+
+format_ensemble(baseline_forecast, forecast_date) %>%
+  write_csv(paste0(model_folder, "/", forecast_date, "-", model_name, ".csv"))
+
+
