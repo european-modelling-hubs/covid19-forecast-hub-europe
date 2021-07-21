5894def5616f86b0bdc7ef702b0d58b54ec00eb9
Hugo Gruson <hugo.gruson@protonmail.com>
2021-07-21 13:20:47 +0200

    Add example to prevent R CMD check warning

--- code/R/get_hub_config.R
+++ code/R/get_hub_config.R
@@ -5,6 +5,11 @@
 #' @param config_file Path to the config file
 #'
 #' @export
+#'
+#' @examples
+#' # Get config for European COVID-19 forecast hub
+#' get_hub_config(config_file = "https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/forecasthub.yml")
+#'
 get_hub_config <- function(setting, config_file = here::here("forecasthub.yml")) {
 
   if (missing(setting)) {
