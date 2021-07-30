2be7b9bdf115264a4d71bb6aa52c97977d926faf
Hugo Gruson <hugo.gruson@protonmail.com>
2021-07-30 15:34:28 +0200

    Add hospitalizations to truth_to_plot.csv

--- viz/prepare_truth_data.R
+++ viz/prepare_truth_data.R
@@ -8,7 +8,11 @@ df_case <- read_csv(here("data-truth/JHU/truth_JHU-Incident Deaths.csv")) %>%
 df_death <- read_csv(here("data-truth/JHU/truth_JHU-Incident Cases.csv")) %>%
   rename(inc_case = value)
 
+df_hosp <- read_csv(here("data-truth/ECDC/truth_ECDC-Incident Hospitalizations.csv")) %>%
+  rename(inc_hosp = value)
+
 df <- full_join(df_case, df_death, by = c("date", "location", "location_name"))
+df <- full_join(df, df_hosp, by = c("date", "location", "location_name"))
 
 df <- df %>%
   # add epi weeks for aggregation
@@ -19,12 +23,13 @@ df <- df %>%
   # aggregate to weekly incidence
   summarise(date = max(date),
             inc_death = sum(inc_death),
-            inc_case = sum(inc_case)) %>%
+            inc_case = sum(inc_case),
+            inc_hosp = sum(inc_hosp)) %>%
   ungroup() %>%
   # only keep Saturdays
   filter(lubridate::wday(date, label = TRUE) == "Sat") %>%
   # reformat
-  select(date, location, location_name, inc_case, inc_death) %>%
+  select(date, location, location_name, inc_case, inc_death, inc_hosp) %>%
   arrange(date, location)
 
 write_csv(df, "viz/truth_to_plot.csv", quote = "needed")
