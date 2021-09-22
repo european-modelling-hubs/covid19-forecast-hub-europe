33d90d87b266f91e467abbde5f1d8d3e2c475f3d
kathsherratt <kathsherratt@gmail.com>
2021-09-22 06:05:01 +0100

    weights

--- code/papers/ensemble-eval/R/get-eval.R
+++ code/papers/ensemble-eval/R/get-eval.R
@@ -1,49 +1,61 @@
-# Get evaluation dataset
 library(here)
-library(purrr)
 library(readr)
 library(dplyr)
 library(tidyr)
-library(ggplot2)
-library(covidHubUtils)
-library(EuroForecastHub)
 
-# path to this folder of main hub repo - for saving figures
-file_path <- here("code", "papers", "ensemble-eval", "/")
+# Set up ------------------------------------------------------------------
+max_date <- "2021-08-23"
 
+# set path to this folder of main hub repo 
+file_path_ensemble <- here("code", "papers", "ensemble-eval", "/")
+
+# If not already, run evaluation on all ensemble files:
+# source(here(file_path_ensemble, "R", "re-run-eval.R"))
+
+# Get evaluation dataset --------------------------------------------------
 # use freshly run evaluation saved in /data folder:
-eval_file <- here("code", "papers", "ensemble-eval", "data", "2021-08-23-evaluation-all-ensembles.csv")
+eval_ensemble <- here(file_path_ensemble, "data", 
+                           paste0(max_date, "-evaluation-all-ensembles.csv"))
+eval_ensemble <- read_csv(eval_ensemble)
 
-# clean variable names
-clean_variables <- c("inc case" = "Cases", "inc death" = "Deaths")
+# Prep dataset ------------------------------------------------------------
+# neater categorical variable names
+clean_variable_names <- c("inc case" = "Cases", "inc death" = "Deaths")
+clean_ensemble_names <- c(
+  "mean" = "Mean",
+  "relative_skill_weighted_mean" = "Weighted mean",
+  "relative_skill_weighted_mean_by_horizon" = "Weighted mean by horizon",
+  "median" = "Median",
+  "relative_skill_weighted_median" = "Weighted median",
+  "relative_skill_weighted_median_by_horizon" = "Weighted median by horizon"
+)
+clean_ensemble_type <- c(
+  "mean" = "Mean",
+  "relative_skill_weighted_mean" = "Mean",
+  "relative_skill_weighted_mean_by_horizon" = "Mean",
+  "median" = "Median",
+  "relative_skill_weighted_median" = "Median",
+  "relative_skill_weighted_median_by_horizon" = "Median"
+)
 
-# Get evaluation and tidy up
-eval <- read_csv(eval_file) %>%
+
+# Tidy up
+eval_ensemble <- eval_ensemble %>%
   # keep only 1-4 horizons
   filter(horizon <= 4) %>%
   # clean up team-model names
   separate(model, into = c("team_name", "model_name"), 
            sep = "-", remove = FALSE) %>%  
   mutate(
-    # add neat variables, esp useful for plots
-    target_variable = recode(target_variable, !!!clean_variables),
+    target_variable = recode(target_variable, !!!clean_variable_names),
+    ensemble_type = recode(model_name, !!!clean_ensemble_type),
+    ensemble_name = recode(model_name, !!!clean_ensemble_names),
     # ensure scores are numeric
-    across(c(horizon, interval_score:n), as.numeric))
-
-
-# separate out baseline as comparator
-score_base <- eval %>%
-  filter(grepl("hub-baseline", model)) %>%
-  select(baseline_score = interval_score,
-         baseline_rel_wis = rel_wis,
-         baseline_rel_ae = rel_ae,
-         target_variable, horizon, location)
-
-eval_wide <- eval %>%
-  filter(!grepl("hub-baseline", model)) %>%
-  full_join(score_base)
-
-###
-rm(clean_variables,
-   score_base, score_ensemble)
-   
+    across(c(horizon, interval_score:n), as.numeric),
+    # set horizon as ordered factor
+    horizon = factor(horizon, ordered = TRUE)) %>%
+  select(ensemble = model_name, ensemble_name, ensemble_type,
+         location, location_name, target_variable, horizon,
+         n, rel_wis, cov_50, cov_95) %>%
+  filter(!location %in% "Overall" &
+           ensemble != "baseline")
--- code/papers/ensemble-eval/R/weights.R
+++ code/papers/ensemble-eval/R/weights.R
@@ -1,6 +1,6 @@
 # Plot components + weights of contributing models
 
-# run: create-all-methods-ensembles.R
+# origin: create-all-methods-ensembles.R
 
 # Get evaluation dataset
 library(here)
@@ -13,160 +13,135 @@ library(ggplot2)
 library(covidHubUtils)
 library(EuroForecastHub)
 
-# path to this folder of main hub repo - for saving figures
-file_path <- here("code", "papers", "ensemble-eval", "/")
-ensemble_names <- c(
-  "mean" = "Mean",
-  "median" = "Median",
-  "relative_skill_weighted_mean" = "Weighted mean",
-  "relative_skill_weighted_mean_by_horizon" = "Weighted mean by horizon",
-  "relative_skill_weighted_median" = "Weighted median",
-  "relative_skill_weighted_median_by_horizon" = "Weighted median by horizon"
-  )
+# get some cleaning variables
+source(here::here("code", "papers", "ensemble-eval", "R", "get-eval.R"))
+# max_date <- "2021-08-23"
   
 # get weights -------------------------------------------------------------
-weights_files <- dir(here("ensembles", "weights"), 
-                     recursive = TRUE, full.names = FALSE)
-names(weights_files) <- 1:152
+# Pre-cleaning weights: saved for quicker loading
+# weights_files <- dir(here("ensembles", "weights"),
+#                      recursive = TRUE, full.names = FALSE)
+# names(weights_files) <- 1:152
+# 
+# # read using purrr as files have inconsistent columns
+# weights_raw <- map_dfr(here("ensembles", "weights", weights_files),
+#                ~ read_csv(.), .id = "file") %>%
+#   mutate(file = recode(file, !!!weights_files),
+#          target_variable = recode(target_variable, !!!clean_variable_names)) %>%
+#   # get ensemble method and date from file name
+#   separate(file, into = c("ensemble", "forecast_date"),
+#            sep = "/", remove = TRUE) %>%
+#   separate(forecast_date, into = "forecast_date",
+#            sep = "-EuroCOVIDhub", extra = "drop") %>%
+#   mutate(forecast_date = as.Date(forecast_date),
+#          ensemble = gsub("EuroCOVIDhub-", "", ensemble),
+#          ensemble_name = recode(ensemble, !!!clean_ensemble_names)) %>%
+#   left_join(covidHubUtils::hub_locations_ecdc, by = "location") %>%
+#   filter(forecast_date <= as.Date(max_date))
+# 
+# weights <- weights_raw %>%
+#   filter(!grepl("Overall", location) &
+#            forecast_date >= "2021-03-15") %>%
+#   mutate(weighting = case_when(!is.na(horizon) ~ "Weighted by horizon",
+#                               grepl("weighted", ensemble) ~ "Weighted",
+#                               TRUE ~ "Simple"),
+#          horizon = ifelse(is.na(horizon), "All", horizon))
+# 
+# write_csv(weights, here(file_path_ensemble, "data", "weights.csv"))
 
-# read using purrr as files have inconsistent columns
-weights_raw <- map_dfr(here("ensembles", "weights", weights_files), 
-               ~ read_csv(.), .id = "file") %>%
-  mutate(file = recode(file, !!!weights_files),
-         target_variable = recode(target_variable, 
-                                  "inc case" = "Cases", "inc death" = "Deaths")) %>%
-  # get ensemble method and date from file name
-  separate(file, into = c("ensemble", "forecast_date"), 
-           sep = "/", remove = TRUE) %>%
-  separate(forecast_date, into = "forecast_date",
-           sep = "-EuroCOVIDhub", extra = "drop") %>%
-  mutate(forecast_date = as.Date(forecast_date),
-         ensemble = gsub("EuroCOVIDhub-", "", ensemble),
-         ensemble_name = recode(ensemble, !!!ensemble_names)) %>%
-  left_join(covidHubUtils::hub_locations_ecdc, by = "location")
-
-weights <- weights_raw %>%
-  filter(!grepl("Overall", location) &
-           forecast_date >= "2021-03-15") %>%
-  mutate(horizon = ifelse(is.na(horizon), "All", horizon))
+# load pre-saved from code above
+weights <- read_csv(here(file_path_ensemble, "data", "weights.csv"))
 
 weights_wide <- weights %>%
   select(-ensemble_name) %>%
   pivot_wider(values_from = weight, names_from = ensemble)
 
+# Number of component models over time ------------------------------
+# component models over time
+component_models <- weights %>%
+  filter(grepl("mean", ensemble)) %>%
+  group_by(target_variable, ensemble_name, 
+           weighting, horizon, 
+           forecast_date, location_name) %>%
+  summarise(n_models = n(),
+            sum_weights = sum(weight, na.rm = TRUE)) %>%
+  mutate(n_models = ifelse(sum_weights == 0, NA, n_models)) %>%
+  filter(!grepl("by horizon", weighting))
 
-# Plot: number of component models over time ------------------------------
-# show number of models each week
-weights_raw %>%
-  filter(!location == "Overall" &
-           ensemble %in% c("Mean", "Weighted mean")) %>%
-  mutate(ensemble = recode(ensemble, 
-                           "Mean" = "Simple", 
-                           "Weighted mean" = "Weighted")) %>%
-  group_by(target_variable, ensemble, forecast_date, location_name) %>%
-  summarise(n_models = n()) %>%
+component_models_desc <- map(split(component_models, 
+                                   component_models$weighting), 
+                             ~ tibble(
+                               min = min(.$n_models, na.rm = TRUE),
+                               max = max(.$n_models, na.rm = TRUE),
+                               total = nrow(.)))
+
+# component models over time: plot
+plot_component_models <- component_models %>%
   ggplot(aes(x = forecast_date, y = location_name, 
              fill = n_models)) +
   geom_tile() +
   labs(x = NULL, y = NULL,
        fill = "Number of component models") +
+  scale_fill_viridis_c(direction = -1, 
+                       breaks = seq(0,18, by = 3),
+                       na.value = 0) +
   scale_x_date(date_breaks = "6 weeks",
                date_labels = "%b") +
-  facet_grid(cols = vars(ensemble),
-             rows = vars(target_variable),
-             scales = "free") +
+  facet_wrap(facets = vars(target_variable, weighting),
+             scales = "fixed", dir = "h",
+             labeller = label_wrap_gen(multi_line = FALSE)) +
   theme_bw() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        strip.background = element_blank())
 
-ggsave(here(file_path, "figures", "n-component-models.png"),
-       height = 10)
+#view
+plot_component_models
+#save
+ggsave(here(file_path_ensemble, "figures", "n-component-models.png"),
+       height = 8)
+
 
 # average weight by model per ensemble method --------------------------
 
-# average over all time and all locations
-model_average <- weights %>%
-  group_by(ensemble, model, target_variable, horizon) %>%
-  summarise(n_weights = n(),
-            mean_weight = mean(weight, na.rm = TRUE))
+# average over all time by location
+simple_to_weighted <- weights %>%
+  filter(grepl("(m|M)ean", ensemble_name) &
+           horizon == "All") %>%
+  mutate(ensemble = factor(ensemble)) %>% # alphabetical: mean then relative_...
+  group_by(model, forecast_date, target_variable, location) %>%
+  mutate(change_to_weighted = weight - lag(weight)) %>%
+  ungroup() %>%
+  filter(!is.na(change_to_weighted)) %>%
+  group_by(model, target_variable, location) %>%
+  summarise(change_to_weighted = mean(change_to_weighted, na.rm = TRUE),
+            n = n())
 
 # Plot average weight for models weighted by skill
-model_average %>%
-  filter(grepl("Weighted", ensemble)) %>%
-  ggplot(aes(x = model, y = mean_weight,
-             colour = horizon, fill = horizon)) +
-  geom_point() +
-  facet_grid(rows = vars(target_variable),
-             cols = vars(ensemble))
-
-# weighted by skill: average over all locations, split over time, ignore weight by horizon
-model_average_date <- weights %>%
-  filter(!grepl("by horizon", ensemble) &
-           grepl("Weighted", ensemble)) %>%
-  group_by(ensemble, model, target_variable, 
-           forecast_date) %>%
-  summarise(n_weights = n(),
-            mean_weight = mean(weight, na.rm = TRUE))
-
-# Plot weight over time, highlighting only those with standard deviation > 0.03
-model_average_date %>%
-  ggplot(aes(x = forecast_date, y = mean_weight,
-             colour = model, fill = model)) +
-  geom_point() +
-  geom_line() +
-  gghighlight::gghighlight(sd(mean_weight) > 0.03,
-                           use_group_by = TRUE,
-                           calculate_per_facet = TRUE,
-                           use_direct_label = FALSE) +
-  facet_grid(rows = vars(target_variable),
-             cols = vars(ensemble)) +
-  labs(x = NULL, y = "Model weight") +
+# Average difference in contribution to ensemble,
+# for each model, location, and target, between simple and weighted 
+# Distribution of difference for model, for each country
+plot_simple_to_weighted <- simple_to_weighted %>%
+  ggplot(aes(x = model, y = change_to_weighted)) +
+  geom_boxplot(alpha = 0.8,
+               outlier.alpha = 0.2,
+               aes(colour = target_variable, fill = target_variable)) +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  scale_colour_brewer(palette = "Set1", aesthetics = c("colour", "fill")) +
+  labs(y = "Difference of skill-based weight",
+       x = NULL, fill = NULL, colour = NULL) +
   theme_bw() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 30, hjust = 1),
+        plot.margin = unit(c(0.1,0.1,0.1,1), "cm"))
+#view
+plot_simple_to_weighted
+
+#save
+ggsave(here(file_path_ensemble, "figures", "weights-simple-to-weighted.png"),
+            height = 4, width = 8,
+       plot = plot_simple_to_weighted)
 
 
-# Difference between simple and weighted mean over time ---------------------
-
-# Ignoring weights by horizon
-weights_diff <- weights_wide %>%
-  filter(horizon == "All") %>%
-  mutate(diff_mean = relative_skill_weighted_mean - mean,
-         diff_median = relative_skill_weighted_median - median) %>%
-  select(model, target_variable, location_name,
-         forecast_date,
-         mean, median, diff_mean, diff_median)
-  
-weights_diff %>%
-  ggplot(aes()) +
-  facet_wrap(~ target_variable)
-
-
-# plot --------------------------------------------------------------------
-
-# Show weights by country
-weights %>%
-  filter(target_variable == "Cases" &
-           !location == "Overall") %>%
-  ggplot(aes(x = forecast_date, y = weight, 
-             fill = model, colour = model)) +
-  geom_col() +
-  facet_grid(rows = vars(location),
-             cols = vars(ensemble),
-             scales = "free") +
-  theme(legend.position = "bottom")
-
-
-
-# questions:
-# do weights change more over time or by location?
-# how much do weights differ by model?
-
-
-
-
-
-
-
-
-
+# Description -------------------------------------------------------------
 
 
