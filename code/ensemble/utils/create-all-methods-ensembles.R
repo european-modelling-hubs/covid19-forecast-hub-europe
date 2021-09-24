ba7106416f11015fb6f38ac965ea7bf9fafded0a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-09-24 10:44:21 +0100

    test relative WIS cutoff

--- code/ensemble/utils/create-all-methods-ensembles.R
+++ code/ensemble/utils/create-all-methods-ensembles.R
@@ -9,44 +9,58 @@ library(EuroForecastHub)
 exclude_by_date <- vroom(here("code", "ensemble", "EuroCOVIDhub",
                       "manual-exclusions.csv"))
 
-# Get all past weeks' forecast dates
-all_dates <- vroom(here("code", "ensemble", "EuroCOVIDhub",
-                             "method-by-date.csv")) %>%
-  pull(forecast_date)
+for (cutoff in c(TRUE, FALSE)) {
 
-# Get all methods
-all_methods <- sub("^.*-", "", dir(here("ensembles", "data-processed")))
+  ## Get all past weeks' forecast dates
+  all_dates <- vroom(here("code", "ensemble", "EuroCOVIDhub",
+                          "method-by-date.csv")) %>%
+    pull(forecast_date)
 
-# Run all ensembles for all past dates ------------------------------------
-ensembles <- run_multiple_ensembles(forecast_dates = all_dates,
-                                    methods = all_methods, # all_methods,
-                                    exclude_models = exclude_by_date,
-                                    verbose = TRUE) %>%
+  if (cutoff) {
+    all_dates <- all_dates[-seq(1, 7)]
+  }
+
+  ## Get all methods
+  all_methods <- c("mean", "median", paste0("relative_skill_weighted_", c("mean", "median"), c("", "_by_horizon")))
+
+  ## Run all ensembles for all past dates ------------------------------------
+  ensembles <- run_multiple_ensembles(forecast_dates = all_dates,
+                                      methods = all_methods, # all_methods,
+                                      exclude_models = exclude_by_date,
+                                      verbose = TRUE,
+                                      rel_wis_cutoff = if_else(cutoff, 1, Inf),
+                                      identifier = if_else(cutoff, "cutoff", "")) %>%
   transpose() %>%
   simplify_all()
 
-results <- ensembles$result %>%
-  compact()
+  results <- ensembles$result %>%
+    compact()
 
-# Save in ensemble/data-processed/model
-res <- lapply(all_methods, function(x)
-  suppressWarnings(dir.create(here("ensembles", "data-processed",
-                                   paste0("EuroCOVIDhub-", x)),
-                              recursive = TRUE)))
+  ## Save in ensemble/data-processed/model
+  walk(results,
+       ~ suppressWarnings(dir.create(here("ensembles", "data-processed",
+                                          paste0("EuroCOVIDhub-", .x$method)),
+                                     recursive = TRUE)))
 
-walk(results,
-     ~ vroom_write(x = .x$ensemble,
-                   path = here("ensembles", "data-processed",
-                               paste0("EuroCOVIDhub-", .x$method),
-                               paste0(.x$forecast_date, "-EuroCOVIDhub-",
-                                      .x$method, ".csv")),
-                   delim = ","))
+  walk(results,
+       ~ vroom_write(x = .x$ensemble,
+                     path = here("ensembles", "data-processed",
+                                 paste0("EuroCOVIDhub-", .x$method),
+                                 paste0(.x$forecast_date, "-EuroCOVIDhub-",
+                                        .x$method, ".csv")),
+                     delim = ","))
 
-# Save weights in ensemble/weights/model
-walk(results,
-     ~ vroom_write(x = .x$weights,
-                   path = here("ensembles", "weights",
-                               paste0("EuroCOVIDhub-", .x$method),
-                               paste0(.x$forecast_date, "-EuroCOVIDhub-",
-                                      .x$method, ".csv")),
-                   delim = ","))
+  ## Save weights in ensemble/weights/model
+  walk(results,
+       ~ suppressWarnings(dir.create(here("ensembles", "weights",
+                                          paste0("EuroCOVIDhub-", .x$method)),
+                                     recursive = TRUE)))
+
+  walk(results,
+       ~ vroom_write(x = .x$weights,
+                     path = here("ensembles", "weights",
+                                 paste0("EuroCOVIDhub-", .x$method),
+                                 paste0(.x$forecast_date, "-EuroCOVIDhub-",
+                                        .x$method, ".csv")),
+                     delim = ","))
+}
