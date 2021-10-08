cd722d00aa5fb36ab1bf3be78352e64b25c0d5cd
kathsherratt <kathsherratt@gmail.com>
2021-10-08 15:16:02 +0100

    Update compile-models-report.r

--- code/reports/compile-models-report.r
+++ code/reports/compile-models-report.r
@@ -4,36 +4,39 @@ library(here)
 library(readr)
 library(scoringutils)
 library(rmarkdown)
-library(covidHubUtils)
 library(lubridate)
 library(EuroForecastHub)
 
+# Set parameters
 options(knitr.duplicate.label = "allow")
 
 report_date <- today()
 wday(report_date) <- get_hub_config("forecast_week_day")
 
 suppressWarnings(dir.create(here::here("html")))
+suppressWarnings(dir.create(here::here("html", "report-model-files")))
 
 models <- covidHubUtils::get_model_designations(source = "local_hub_repo",
                                                 hub_repo_path = here()) %>%
   filter(designation %in% c("primary", "secondary")) %>%
   pull(model)
 
-########TODO!! remove
-# models <- models[8:10]
-########
+models <- models[c(14,20,38)]
 
-for (model in models) {
-  rmarkdown::render(here::here("code", "reports", "models",
-                               "model-report.Rmd"),
-                    params = list(model = model,
-                                  report_date = report_date,
-                                  restrict_weeks = 4),
-                    output_format = "html_document",
-                    output_file =
-                      here::here("html",
-                                 paste0("model-report-",
-                                        model, ".html")),
-                    envir = new.env())
-}
+# Create safe function for rendering report for each model
+safely_render <- purrr::safely(.f = ~ rmarkdown::render(here::here("code", "reports", "models",
+                                                              "model-report.Rmd"),
+                                                   params = list(model = .,
+                                                                 report_date = report_date,
+                                                                 restrict_weeks = 4),
+                                                   output_format = "html_document",
+                                                   output_file =
+                                                     here::here("html",
+                                                                paste0("model-report-",
+                                                                       ., ".html")),
+                                                   envir = new.env()),
+                              otherwise = NA_real_)
+
+# Create report for each model
+render_results <- purrr::map(.x = models, 
+            .f = safely_render)
