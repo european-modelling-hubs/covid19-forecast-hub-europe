3de0dac3e254f48aec9602c9509b1a0679e757dd
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-10-12 11:20:52 +0100

    don't render safely - we want to see errors

--- code/reports/compile-models-report.r
+++ code/reports/compile-models-report.r
@@ -22,23 +22,21 @@ models <- covidHubUtils::get_model_metadata(source = "local_hub_repo",
          !grepl("hub-baseline$", model)) %>%
   pull(model)
 
-# TODO remove (speeds up testing)
-models <- models[c(14,20,38)]
-
-# Create safe function for rendering report for each model
-safely_render <- purrr::safely(.f = ~ rmarkdown::render(here::here("code", "reports", "models",
-                                                              "model-report.Rmd"),
-                                                   params = list(model = .,
-                                                                 report_date = report_date,
-                                                                 restrict_weeks = 4),
-                                                   output_format = "html_document",
-                                                   output_file =
-                                                     here::here("html",
-                                                                paste0("model-report-",
-                                                                       ., ".html")),
-                                                   envir = new.env()),
-                              otherwise = NA_real_)
+# Create function for rendering report for each model
+render_report <- function(model) {
+	rmarkdown::render(here::here("code", "reports", "models",
+                               "model-report.Rmd"),
+                    params = list(model = model,
+                                  report_date = report_date,
+                                  restrict_weeks = 4),
+                    output_format = "html_document",
+                    output_file =
+                      here::here("html",
+                                 paste0("model-report-",
+                                        model, ".html")),
+                    envir = new.env())
+}
 
 # Create report for each model
-purrr::walk(.x = models, 
-            .f = safely_render)
+purrr::walk(.x = models,
+            .f = render_report)
