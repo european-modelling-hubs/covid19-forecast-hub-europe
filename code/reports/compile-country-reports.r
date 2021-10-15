1679610fc29a3cfb3602d1e203803c3dc6603873
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-10-15 20:49:27 +0100

    rename

--- code/reports/compile-country-reports.r
+++ code/reports/compile-country-reports.r
@@ -16,15 +16,15 @@ wday(report_date) <- get_hub_config("forecast_week_day")
 suppressWarnings(dir.create(here::here("html")))
 
 for (country in c("Overall", hub_locations_ecdc$location_name)) {
-  rmarkdown::render(here::here("code", "reports", "evaluation",
-                               "evaluation-report.Rmd"),
+  rmarkdown::render(here::here("code", "reports", "country",
+                               "country-report.Rmd"),
                     output_format = "html_document",
                     params = list(location_name = country,
                                   report_date = report_date,
                                   plot_weeks = 4),
                     output_file =
                       here::here("html",
-                                 paste0("evaluation-report-",
+                                 paste0("country-report-",
                                         country, ".html")),
                     envir = new.env())
 }
