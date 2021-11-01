5fdfc8e5cd77e11665140598c7d4abb46487339e
kathsherratt <kathsherratt@gmail.com>
2021-11-01 01:07:03 +0000

    Create create-data-locations.R

---
+++ code/auto_download/create-data-locations.R
@@ -0,0 +1,13 @@
+# Create data-locations.csv
+library(here)
+library(readr)
+library(dplyr)
+
+hosp <- read_csv(here("code", "auto_download", "hospitalisation-sources.csv")) %>%
+  filter(!is.na(source))
+
+locs <- read_csv(here("data-locations", "locations_eu.csv")) %>%
+  mutate(inc_hosp = case_when(location_name %in% hosp$country ~ "ECDC",
+                              location %in% c("GB", "CH") ~ "covidregionaldata"))
+
+         
