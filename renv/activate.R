b7d9f21a73672123a040783d82c6bde0a4b2116f
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-03-08 14:20:42 +0100

    Update renv.lock

--- renv/activate.R
+++ renv/activate.R
@@ -54,9 +54,19 @@ local({
   # mask 'utils' packages, will come first on the search path
   library(utils, lib.loc = .Library)
 
-  # unload renv if it's already been laoded
-  if ("renv" %in% loadedNamespaces())
+  # check to see if renv has already been loaded
+  if ("renv" %in% loadedNamespaces()) {
+
+    # if renv has already been loaded, and it's the requested version of renv,
+    # nothing to do
+    spec <- .getNamespaceInfo(.getNamespace("renv"), "spec")
+    if (identical(spec[["version"]], version))
+      return(invisible(TRUE))
+
+    # otherwise, unload and attempt to load the correct version of renv
     unloadNamespace("renv")
+
+  }
 
   # load bootstrap tools   
   `%||%` <- function(x, y) {
@@ -148,20 +158,16 @@ local({
     nv <- numeric_version(version)
     components <- unclass(nv)[[1]]
   
-    # if this appears to be a development version of 'renv', we'll
-    # try to restore from github
-    dev <- length(components) == 4L
-  
-    # begin collecting different methods for finding renv
-    methods <- c(
-      renv_bootstrap_download_tarball,
-      if (dev)
+    methods <- if (length(components) == 4L) {
+      list(
         renv_bootstrap_download_github
-      else c(
+      )
+    } else {
+      list(
         renv_bootstrap_download_cran_latest,
         renv_bootstrap_download_cran_archive
       )
-    )
+    }
   
     for (method in methods) {
       path <- tryCatch(method(version), error = identity)
@@ -298,33 +304,6 @@ local({
   
   }
   
-  renv_bootstrap_download_tarball <- function(version) {
-  
-    # if the user has provided the path to a tarball via
-    # an environment variable, then use it
-    tarball <- Sys.getenv("RENV_BOOTSTRAP_TARBALL", unset = NA)
-    if (is.na(tarball))
-      return()
-  
-    # allow directories
-    info <- file.info(tarball, extra_cols = FALSE)
-    if (identical(info$isdir, TRUE)) {
-      name <- sprintf("renv_%s.tar.gz", version)
-      tarball <- file.path(tarball, name)
-    }
-  
-    # bail if it doesn't exist
-    if (!file.exists(tarball))
-      return()
-  
-    fmt <- "* Bootstrapping with tarball at path '%s'."
-    msg <- sprintf(fmt, tarball)
-    message(msg)
-  
-    tarball
-  
-  }
-  
   renv_bootstrap_download_github <- function(version) {
   
     enabled <- Sys.getenv("RENV_BOOTSTRAP_FROM_GITHUB", unset = "TRUE")
@@ -378,13 +357,7 @@ local({
     bin <- R.home("bin")
     exe <- if (Sys.info()[["sysname"]] == "Windows") "R.exe" else "R"
     r <- file.path(bin, exe)
-  
-    args <- c(
-      "--vanilla", "CMD", "INSTALL", "--no-multiarch",
-      "-l", shQuote(path.expand(library)),
-      shQuote(path.expand(tarball))
-    )
-  
+    args <- c("--vanilla", "CMD", "INSTALL", "--no-multiarch", "-l", shQuote(library), shQuote(tarball))
     output <- system2(r, args, stdout = TRUE, stderr = TRUE)
     message("Done!")
   
@@ -758,17 +731,12 @@ local({
   
   }
   
-  renv_bootstrap_user_dir <- function() {
-    dir <- renv_bootstrap_user_dir_impl()
-    path.expand(chartr("\\", "/", dir))
+  renv_bootstrap_user_dir <- function(path) {
+    dir <- renv_bootstrap_user_dir_impl(path)
+    chartr("\\", "/", dir)
   }
   
-  renv_bootstrap_user_dir_impl <- function() {
-  
-    # use local override if set
-    override <- getOption("renv.userdir.override")
-    if (!is.null(override))
-      return(override)
+  renv_bootstrap_user_dir_impl <- function(path) {
   
     # use R_user_dir if available
     tools <- asNamespace("tools")
@@ -779,8 +747,10 @@ local({
     envvars <- c("R_USER_CACHE_DIR", "XDG_CACHE_HOME")
     for (envvar in envvars) {
       root <- Sys.getenv(envvar, unset = NA)
-      if (!is.na(root))
-        return(file.path(root, "R/renv"))
+      if (!is.na(root)) {
+        path <- file.path(root, "R/renv")
+        return(path)
+      }
     }
   
     # use platform-specific default fallbacks
@@ -792,7 +762,6 @@ local({
       "~/.cache/R/renv"
   
   }
-  
   
   renv_json_read <- function(file = NULL, text = NULL) {
   
