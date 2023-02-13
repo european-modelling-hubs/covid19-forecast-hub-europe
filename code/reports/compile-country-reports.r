35a2336051118ee3199c421d65ad63f5ea7b91b4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-02-13 11:55:38 +0000

    define models in country reports

--- code/reports/compile-country-reports.r
+++ code/reports/compile-country-reports.r
@@ -16,6 +16,25 @@ wday(report_date) <- get_hub_config("forecast_week_day")
 
 suppressWarnings(dir.create(here::here("html")))
 
+models <- list.files(
+  here::here("model-metadata"),
+  full.names = TRUE
+) |>
+  purrr::map(yaml::read_yaml) |>
+  purrr::map_chr("model_abbr")
+
+last_4_forecast_dates <- report_date - weeks(seq(1, 4))
+
+recently_submitted <- models |>
+  purrr::map_lgl(\(model) any(file.exists(
+    here::here("data-processed", model, paste0(
+      last_4_forecast_dates, "-", model, ".csv"
+    )
+  )))
+)
+
+models <- models[recently_submitted]
+
 for (country in c("Overall", hub_locations_ecdc$location_name)) {
   message("Generating report for ", country)
   rmarkdown::render(here::here("code", "reports", "country",
@@ -23,7 +42,8 @@ for (country in c("Overall", hub_locations_ecdc$location_name)) {
                     output_format = "html_document",
                     params = list(location_name = country,
                                   report_date = report_date,
-                                  plot_weeks = 4),
+                                  plot_weeks = 4,
+                                  report_models = models),
                     output_file =
                       here::here("html",
                                  paste0("country-report-",
