70755867b6c2375d33a423f0e54c30da3b73fb31
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-02-14 21:05:24 +0000

    update data locations

--- code/auto_download/create-data-locations.R
+++ code/auto_download/create-data-locations.R
@@ -4,10 +4,10 @@ library(readr)
 library(dplyr)
 library(covidHubUtils)
 
-hosp <- read_csv(here("data-truth", "ECDC", "truth_ECDC-Incident Hospitalizations.csv"))
+hosp <- read_csv(here("data-truth", "OWID", "truth_OWID-Incident Hospitalizations.csv"))
 
 locs <- covidHubUtils::hub_locations_ecdc %>%
-  mutate(inc_hosp = case_when(location_name %in% hosp$location_name ~ "ECDC"),
+  mutate(inc_hosp = case_when(location_name %in% hosp$location_name ~ "OWID"),
          inc_case = "JHU",
          inc_death = "JHU")
 
