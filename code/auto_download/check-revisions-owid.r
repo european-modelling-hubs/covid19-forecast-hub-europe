c035ed559485f56bb369f8bbc1141bdbe69c6053
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-02-14 14:55:40 +0000

    update cutoff processing

--- code/auto_download/check-revisions-owid.r
+++ code/auto_download/check-revisions-owid.r
@@ -9,29 +9,30 @@ library("vroom")
 latest_date <- lubridate::today()
 wday(latest_date) <- EuroForecastHub::get_hub_config("forecast_week_day")
 
-data_dir <- here::here("data-truth", "OWID", "snapshots")
+owid_dir <- here::here("data-truth", "OWID")
+snapshot_dir <- file.path(owid_dir, "snapshots")
+final_dir <- file.path(owid_dir, "final")
 
-owid_files <- list.files(data_dir, pattern = "20")
+snapshot_files <- list.files(snapshot_dir, pattern = "20")
+snapshot_dates <- as.Date(
+  sub("^.*(20[0-9]{2}-[0-9]{2}-[0-9]{2}).*$", "\\1", snapshot_files)
+)
 
-owid_revisions <- tibble::tibble(file = owid_files) |>
-  dplyr::mutate(dl_date = as.Date(
-    sub("^.*(20[0-9]{2}-[0-9]{2}-[0-9]{2}).*$", "\\1", file))
-  ) |>
-  dplyr::filter(
-    wday(dl_date, label = TRUE, abbr = FALSE) ==
-      EuroForecastHub::get_hub_config("forecast_week_day")
-  ) |>
+combine_files <- function(date) {
+  final <- readr::read_csv(file.path(
+    final_dir, paste0("covid-hospitalizations-final_", date - days(27), ".csv")
+  ), show_col_types = FALSE)
+  snapshot <- readr::read_csv(file.path(
+    snapshot_dir, paste0("covid-hospitalizations_", date, ".csv")
+  ), show_col_types = FALSE) |>
+    filter(date > {{ date }} - days(28)) |>
+    mutate(snapshot_date = {{ date }})
+  return(list(dplyr::bind_rows(final, snapshot)))
+}
+
+owid_revisions <- tibble::tibble(dl_date = snapshot_dates) |>
   dplyr::rowwise() |>
-  dplyr::mutate(data = list(vroom::vroom(
-    file = file.path(data_dir, file),
-    col_types = c(
-      location_name = "c",
-      location = "c",
-      date = "D",
-      value = "i",
-      source = "c"
-    )
-  ))) |>
+  dplyr::mutate(data = combine_files(dl_date)) |>
   dplyr::ungroup() |>
   tidyr::unnest(data) |>
   dplyr::filter(dl_date > max(dl_date) - weeks(12)) |>
@@ -39,16 +40,10 @@ owid_revisions <- tibble::tibble(file = owid_files) |>
   dplyr::mutate(max_date = max(date)) |>
   dplyr::ungroup() |>
   dplyr::mutate(weeks_back = ceiling(as.numeric(max_date - date + 1) / 7)) |>
-  dplyr::filter(weeks_back <= 5) |>
   dplyr::group_by(date, location_name, location) |>
   dplyr::mutate(revision = (max(value) - value) / value) |>
   dplyr::group_by(location, location_name, weeks_back) |>
   dplyr::summarise(mean_relative_revision = mean(revision), .groups = "drop")
-
-vroom::vroom_write(
-  owid_revisions,
-  here::here("data-truth", "OWID", "revisions.csv"), delim = ","
-)
 
 recommended_cutoffs <- owid_revisions |>
   dplyr::filter(mean_relative_revision > 0.05) |>
@@ -62,7 +57,7 @@ recommended_cutoffs <- owid_revisions |>
   dplyr::group_by(location, location_name) |>
   dplyr::filter(weeks_back == max(weeks_back)) |>
   dplyr::ungroup() |>
-  dplyr::select(location, location_name, cutoff = weeks_back)
+  dplyr::select(location, location_name, cutoff_weeks = weeks_back)
 
 vroom::vroom_write(
   recommended_cutoffs,
