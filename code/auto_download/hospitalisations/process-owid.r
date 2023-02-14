4ae87bb0297cf78280315ad16286f614ed310888
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-02-14 19:35:18 +0000

    update format

--- code/auto_download/hospitalisations/process-owid.r
+++ code/auto_download/hospitalisations/process-owid.r
@@ -1,5 +1,6 @@
 library("readr")
 library("lubridate")
+library("purrr")
 
 cat("Processing OWID data.\n")
 
@@ -30,16 +31,20 @@ if (length(final_files) > 0) {
   final_dates <- final_dates[!final_dates_present]
 }
 
+if (length(final_dates) == 0) {
+  stop("No new data to process." )
+}
+
 if (min(final_dates) == min(snapshot_dates) - days(28) + 1) {
   ## need to create first "final" dataset
-  init <- read_csv(
+  init <- readr::read_csv(
     file.path(
       snapshot_dir, grep(min(snapshot_dates), snapshot_files, value = TRUE)
     ), show_col_types = FALSE
   ) |>
-    mutate(snapshot_date = min(snapshot_dates)) |>
-    filter(date <= min_snapshot - days(cutoff_days))
-  write_csv(df, file.path(
+    dplyr::mutate(snapshot_date = min(snapshot_dates)) |>
+    dplyr::filter(date <= min_snapshot - days(cutoff_days))
+  readr::write_csv(df, file.path(
     final_dir, paste0(
       "covid-hospitalizations-final_",
       min(final_dates),
@@ -53,18 +58,18 @@ if (min(final_dates) == min(snapshot_dates) - days(28) + 1) {
 snapshot_dates <- snapshot_dates[snapshot_dates >= min(final_dates)]
 
 ## create snapshot dataset
-snapshots <- map(snapshot_dates,
+snapshots <- purrr::map(snapshot_dates,
   \(x) read_csv(
     file.path(snapshot_dir, grep(x, snapshot_files, value = TRUE)),
     show_col_types = FALSE
   ) |>
-    mutate(snapshot_date = x)
+    dplyr::mutate(snapshot_date = x)
 ) |>
-  bind_rows()
+  dplyr::bind_rows()
 
 for (final_date_chr in as.character(final_dates)) {
   final_date <- as.Date(final_date_chr)
-  init <- read_csv(
+  init <- readr::read_csv(
     file.path(
       final_dir, paste0(
         "covid-hospitalizations-final_", final_date - days(1), ".csv"
@@ -73,21 +78,28 @@ for (final_date_chr in as.character(final_dates)) {
     show_col_types = FALSE
   )
   df <- init |>
-    bind_rows(snapshots) |>
-    filter(
+    dplyr::bind_rows(snapshots) |>
+    dplyr::filter(
       snapshot_date < final_date + days(cutoff_days),
       snapshot_date >= date + days(cutoff_days)
     ) |>
-    group_by(date) |>
-    filter(snapshot_date == min(snapshot_date)) |>
-    ungroup() |>
-    distinct()
-  write_csv(df, file.path(
+    dplyr::group_by(date) |>
+    dplyr::filter(snapshot_date == min(snapshot_date)) |>
+    dplyr::ungroup() |>
+    dplyr::distinct()
+  readr::write_csv(df, file.path(
     final_dir, paste0(
       "covid-hospitalizations-final_", final_date, ".csv"
     ))
   )
 }
+
+## create master data file
+
+recommended_cutoffs <- readr::read_csv(
+  file.path(owid_dir, "recommended-cutoffs.csv"),
+  show_col_types = FALSE
+)
 
 latest_final <- read_csv(
   file.path(
@@ -99,13 +111,20 @@ latest_final <- read_csv(
 )
 
 latest_snapshot <- snapshots |>
-  filter(snapshot_date == max(snapshot_date))
+  dplyr::filter(snapshot_date == max(snapshot_date))
 
 new_data <- latest_snapshot |>
-  anti_join(
+  dplyr::anti_join(
     latest_final, by = c("location_name", "location", "date", "source")
   ) |>
-  mutate(status = "preliminary")
+  dplyr::left_join(recommended_cutoffs, by = c("location_name", "location")) |>
+  tidyr::replace_na(list(cutoff_weeks = 0)) |>
+  dplyr::group_by(location_name, location, source) |>
+  dplyr::mutate(status = dplyr::if_else(
+    floor(as.integer(max(date) - date) / 7) < cutoff_weeks,
+    "expecting revisions", "near final"
+  )) |>
+  dplyr::select(-cutoff_weeks)
 
 df <- latest_final |>
   mutate(status = "final") |>
