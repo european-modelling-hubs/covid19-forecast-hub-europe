c108d1e8311c93a89c77a0bb6815b222f3cab981
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-01-14 23:48:29 +0000

    add legacy truth data to plotting

--- viz/prepare_truth_data.R
+++ viz/prepare_truth_data.R
@@ -6,15 +6,26 @@ library(tidyr)
 
 # FIXME: find a way to get this information directly from the config file
 # without hardcoding target types
-truth <- c(
+var_files <- c(
   "inc_death" = "ECDC/truth_ECDC-Incident Deaths.csv",
   "inc_case" = "ECDC/truth_ECDC-Incident Cases.csv",
   "inc_hosp" = "OWID/truncated_OWID-Incident Hospitalizations.csv"
-) |>
+)
+truth <- var_files |>
   imap(~ read_csv(here("data-truth", .x)) %>% mutate(name = .y)) |>
   bind_rows()
 
+# get legacty data
+legacy <- c("ECDC/final/", "OWID/final") |>
+  map(~ tail(list.files(here("data-truth", .x), full.names = TRUE), 1)) |>
+  map(~ read_csv(.x, show_col_types = FALSE)) |>
+  bind_rows() |>
+  mutate(name = gsub(" ", "_", target_variable)) |>
+  filter(name %in% names(var_files)) |>
+  anti_join(truth, by = c("location_name", "location", "date", "name"))
+
 truth <- truth |>
+  bind_rows(legacy) |>
   # add epi weeks for aggregation
   mutate(date = lubridate::ymd(date),
          epi_week = lubridate::epiweek(date),
