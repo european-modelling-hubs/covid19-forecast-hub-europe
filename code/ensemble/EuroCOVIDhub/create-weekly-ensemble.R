0758d3151fdfedf16a049a97663da2147b3bb6aa
Corrado Gioannini <1991491+gioco@users.noreply.github.com>
2024-09-06 16:21:30 +0200

    Test fixed date exec

--- code/ensemble/EuroCOVIDhub/create-weekly-ensemble.R
+++ code/ensemble/EuroCOVIDhub/create-weekly-ensemble.R
@@ -13,7 +13,8 @@ library(tidyr)
 method <- get_hub_config("ensemble")[["method"]]
 
 # Set current submission date
-forecast_date <- today()
+# forecast_date <- today()
+forecast_date <- "2024-07-01"
 wday(forecast_date) <- get_hub_config("forecast_week_day")
 
 # Get model names for manual exclusion
